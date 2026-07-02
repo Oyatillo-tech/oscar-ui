7f71603a59dd7e6572cad39489bd372e1a1be205
deJony <156835236+Oyatillo-tech@users.noreply.github.com>
2026-07-02 15:49:04 +0500

    adddd

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -30,13 +30,13 @@ export function LocationPage() {
             </div>
           </div>
           <div className="w-full h-[220px] bg-slate-100 relative">
-            {/* <iframe
+            <iframe
               src="https://www.google.com/maps?q=Orikzor+bozori,Tashkent&output=embed"
               className="w-full h-full border-0 absolute inset-0"
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-            ></iframe> */}
+            ></iframe>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.1763082705293!2d69.1588695!3d41.348521399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8fd2de145789%3A0xbcd3702d40da62bc!2sOriginal%20Colormix%20LLC!5e0!3m2!1sru!2s!4v1778621184900!5m2!1sru!2s"
               className="w-full h-full border-0 absolute inset-0"
